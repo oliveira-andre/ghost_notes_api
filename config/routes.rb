fe8379a5619336a863531a17ccfcf090d0a6dc53
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-06 15:37:20 -0300

    feat: generic db config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :notes, only: %i[create destroy], param: :slug do
+    get :password
+  end
 end
