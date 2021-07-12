f804905b13f244720686b676a71f3db8dd44bb85
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-12 04:38:47 -0300

    feat: rescueing service  exception and adding serializer method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  rescue_from ServiceException do |e|
+    error_serializer(e.message)
+  end
+
+  def serializer(params = {})
+    data = params[:data]
+    serializer = params[:serializer]
+    status = params[:status] || :ok
+
+    if params[:status] == :no_content
+      render json: {}, status: status
+    else
+      render json: serializer.new(data).serialize, status: status
+    end
+  end
+
+  private
+
+  def error_serializer(error_message)
+    render json: { errors: error_message }, status: 422
+  end
 end
