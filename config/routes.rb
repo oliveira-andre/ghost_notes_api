32da620fb3412a1d0b54f69f11d917b62db39eda
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-18 19:04:50 -0300

    feat: routing to show notes with and without password

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :notes, only: %i[create destroy], param: :slug
+      resources :notes, only: %i[create destroy show], param: :slug
+
+      get '/notes/:slug/password/:password' => 'notes#show'
     end
   end
 end
