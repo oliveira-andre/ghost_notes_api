6820b58c6a42d914f1c4af8207bcbb8c7e7019cf
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-20 13:46:13 -0300

    feat: validating uniqueness of slug

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,10 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  subject { build(:note) }
+
   context 'validate presence of' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
     it { should validate_presence_of(:keys) }
-    it { should validate_presence_of(:slug) }
+  end
+
+  context 'validate uniqueness of' do
+    it { should validate_uniqueness_of(:slug) }
   end
 end
